0d36081eaa63c5d6d9964ee1e42e90a5733d3f5c
Seppe Geuens <geuensseppe@gmail.com>
2021-06-10 21:48:07 +0200

    BEM

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,17 @@
 // Navigation
 $(".hamburger").click(function () {
+	$(this).toggleClass("active");
 	$(".nav-menu").toggleClass("active");
-	$(".hamburger").toggleClass("active");
 });
+
+/*------------------------------------*\
+  	const hamburger = document.querySelector(".hamburger");
+	const navMenu = document.querySelector(".nav-menu");
+
+	hamburger.addEventListener("click", mobileMenu);
+
+	function mobileMenu() {
+		hamburger.classList.toggle("active");
+		navMenu.classList.toggle("active");
+	}
+\*------------------------------------*/
